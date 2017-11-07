84cf51647b5f2d8a295ea42634d57d700e4f2bdf
Lizabeth Petersen <lizpetersen928@gmail.com>
2017-11-07 13:09:08 -0800

    adds start of chart code in order to switch branches

--- app.js
+++ app.js
@@ -2,19 +2,31 @@
 
 let clicks = 0;
 
-const banana = new Product('banana', 'banana.jpg');
-const dragon = new Product('dragon', 'dragon.jpg');
-const tauntaun = new Product('tauntaun', 'tauntaun.jpg');
-const dogDuck = new Product('dog-duck', 'dog-duck.jpg');
-const waterCan = new Product('water-can', 'water-can.jpg');
-const pen = new Product('pen', 'pen.jpg');
 const bag = new Product('bag', 'bag.jpg');
+const banana = new Product('banana', 'banana.jpg');
 const bathroom = new Product('bathroom', 'bathroom.jpg');
 const boots = new Product('boots', 'boots.jpg');
 const breakfast = new Product('breakfast', 'breakfast.jpg');
 const bubblegum = new Product('bubblegum', 'bubblegum.jpg');
+const chair = new Product('chair', 'chair.jpg');
+const cthulhu = new Product('cthulhu', 'cthulhu.jpg');
+const dogDuck = new Product('dog-duck', 'dog-duck.jpg');
+const dragon = new Product('dragon', 'dragon.jpg');
+const pen = new Product('pen', 'pen.jpg');
+const petSweep = new Product('pet-sweep', 'pet-sweep.jpg');
+const scissors = new Product('scissors', 'scissors.jpg');
+const shark = new Product('shark', 'shark.jpg');
+const sweep = new Product('sweep', 'sweep.jpg');
+const tauntaun = new Product('tauntaun', 'tauntaun.jpg');
+const unicorn = new Product('unicorn', 'unicorn.jpg');
+const usb = new Product('usb', 'usb.gif');
+const waterCan = new Product('water-can', 'water-can.jpg');
+const wineGlass = new Product('wine-glass', 'wine-glass.jpg');
 
-const products = [banana, dragon, tauntaun, dogDuck, waterCan, pen, bag, bathroom, boots, breakfast, bubblegum];
+
+const products = [bag, banana, chair, cthulhu, dragon, tauntaun, dogDuck, petSweep, scissors, shark, sweep, wineGlass, usb, unicorn, waterCan, pen, bag, bathroom, boots, breakfast, bubblegum];
+console.log(products);
+
 
 for (let i = 0; i < 3; i++); {
     appendRandomProduct();
@@ -59,5 +71,11 @@ function endVote () {
     vote.removeEventListener('click', clickHandler);
 
     console.table(products);
+    drawChart();
 }
-
+function drawChart () {
+    const canvas = document.getElementById('endVote');
+    const context = canvas.getContext('2d');
+    context.fillStyle = 'rbga()';
+    context.
+}
