e02690e64b87e6c8892135f8e08f45728b404ea8
Lizabeth Petersen <lizpetersen928@gmail.com>
2017-11-08 13:54:51 -0800

    Updates file names and updates css/html

---
+++ Product.js
@@ -0,0 +1,28 @@
+'use strict';
+
+
+function Product (name, src) {//eslint-disable-line
+    this.name = name;
+    this.src = './assets/' + src;
+    this.displayed = 0;
+    this.clicks = 0;
+}
+
+Product.prototype.wasDisplayed = function(){
+    this.displayed += 1;
+};
+
+Product.prototype.wasClicked = function() {
+    this.clicks += 1;
+};
+
+Product.prototype.render = function() {
+    const ele = document.createElement('img');
+    ele.src = this.src;
+    ele.classList.add(this.name);
+
+    return ele;
+};
+
+
+console.log(Product);
